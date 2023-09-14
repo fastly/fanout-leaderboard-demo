9cf6aba42a2bb62ee3af675d27fc8025f4d6cfc3
Katsuyuki Omuro <komuro@fastly.com>
2023-09-14 15:58:19 +0900

    Add devhub metadata

--- edge/src/index.js
+++ edge/src/index.js
@@ -5,6 +5,10 @@
 
 /// <reference types="@fastly/js-compute" />
 import { createFanoutHandoff } from 'fastly:fanout';
+import { includeBytes } from 'fastly:experimental';
+
+const demoManifest = includeBytes('/resources/demo-manifest.txt');
+const screenshot = includeBytes('/resources/screenshot.png');
 
 addEventListener('fetch', (event) => event.respondWith(handleRequest(event)));
 
@@ -12,7 +16,28 @@ addEventListener('fetch', (event) => event.respondWith(handleRequest(event)));
  * @param { FetchEvent } event
  */
 async function handleRequest(event) {
+
   if (event.request.method === 'GET') {
+    const { pathname } = new URL(event.request.url);
+
+    if (pathname === '/.well-known/fastly/demo-manifest') {
+      return new Response(demoManifest, {
+        status: 200,
+        headers: {
+          'Content-Type': 'text/plain; charset=utf-8',
+        },
+      });
+    }
+
+    if (pathname === '/images/screenshot.png') {
+      return new Response(screenshot, {
+        status: 200,
+        headers: {
+          'Content-Type': 'image/png',
+        },
+      });
+    }
+
     // If it's a GET request, then we will pass it through Fanout
 
     // The createFanoutHandoff() creates a Response instance
